99005986518800a3fd83141bb5fab2dba13942a6
Reilly <reillyswork@gmail.com>
2023-11-21 00:03:09 -0700

    finished lab

--- server/app.py
+++ server/app.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import Cheese, Producer, db
+from flask_restful import Api, Resource
 
-# from flask_restful import Api, Resource
 
 
 app = Flask(__name__)
@@ -16,13 +16,80 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = Api(app)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    response = make_response({"message": "Hello Fromagers!"}, 200)
-    return response
+#@app.route("/")
+#def index():
+ #   response = make_response({"message": "Hello Fromagers!"}, 200)
+  #  return response
+  
+class ProducerResource(Resource):
+    def get(self):
+        producers = Producer.query.all()
+        response_data = [producer.serialize() for producer in producers]
+        return make_response(jsonify(response_data), 200)
+    
+    def get(self, producer_id):
+        producer = Producer.query.get_or_404(producer_id)
+        response_data = producer.serialize()
+        return make_response(jsonify(response_data), 200)
+    
+    def delete(self, producer_id):
+        producer = Producer.query.get_or_404(producer_id)
+        db.session.delete(producer)
+        db.session.commit()
+        return make_response('', 204)
+    
+class CheeseResource(Resource):
+    def post(self):
+        data = request.get_json()
+        
+        try:
+            producer_id = data["producer_id"]
+            producer = Producer.query.get_or_404(producer_id)
+        except KeyError:
+            return make_response(jsonify({"error": "Invalid request, missing 'producer_id'"}), 400)
+        
+        cheese_data = {
+            "kind": data["kind"],
+            "is_raw_milk": data["is_raw_milk"],
+            "production_date": data["production_date"],
+            "image": data["image"],
+            "price": data["price"],
+            "producer": producer
+        }
+        
+        cheese = Cheese(**cheese_data)
+        
+        db.session.add(cheese)
+        db.session.commit()
+        
+        response_data = cheese.to_dict()
+        return make_response(jsonify(response_data), 201)
+    
+    def patch(self, cheese_id):
+        cheese = Cheese.query.get_or_404(cheese_id)
+        data = request.get_json()
+        
+        try:
+            cheese.is_raw_milk = data["is_raw_milk"]
+            cheese.production_date = data["production_date"]
+        except KeyError:
+            return make_response(jsonify({"error": "Invalid request, missing 'is_raw_milk' or 'production_date'"}), 400)
+        
+        db.session.commit()
+        response_data = cheese.serialize()
+        return make_response(jsonify(response_data), 200)
+    
+    def delete(self, cheese_id):
+        cheese = Cheese.query.get_or_404(cheese_id)
+        db.session.delete(cheese)
+        db.session.commit()
+        return make_response('', 204)
+    
+api.add_resource(ProducerResource, '/producers', '/producers/<int:producer_id>')
+api.add_resource(CheeseResource, '/cheeses', '/cheeses/<int:cheese_id>')
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime 
 
 db = SQLAlchemy()
 
@@ -10,15 +11,61 @@ class Producer(db.Model, SerializerMixin):
     __tablename__ = "producers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    founding_year = db.Column(db.Integer)
+    region = db.Column(db.String(255))
+    operation_size = db.Column(db.String(20))
+    image = db.Column(db.String(255))
 
+    cheeses = db.relationship('Cheese', backref='producer', lazy=True)
+    
+    @validates(name)
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Producer contain name')
+        return name
+    
+    @validates(founding_year)
+    def validate_founding_year(self, key, founding_year):
+        if founding_year < 1900 or founding_year > datetime.utcnow().year:
+            raise ValueError("Founding year must be between 1900 and the current year.")
+        return founding_year
+    
+    @validates(operation_size)
+    def validate_operation_size(self, key, operation_size):
+        valid_sizes = ["small", "medium", "large", "family", "corporate"]
+        if operation_size not in valid_sizes:
+            raise ValueError("Operation size must be one of: small, medium, large, family, corporate")
+        return operation_size
+    
     def __repr__(self):
         return f"<Producer {self.id}>"
-
-
+    
 class Cheese(db.Model, SerializerMixin):
     __tablename__ = "cheeses"
 
     id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(255))
+    producer_id = db.Column(db.Integer, db.ForeignKey('producers.id'))
+    is_raw_milk = db.Column(db.Boolean)
+    production_date = db.Column(db.DateTime, default=datetime.utcnow)
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    
+    producer_name = association_proxy('producer', 'name')
+    
+    @validates(production_date)
+    def validate_production_date(self, key, production_date):
+        if production_date > datetime.utcnow():
+            raise ValueError("Production date must be before today")
+        return production_date
+ 
+    @validates(price)
+    def validate_price(self, key, price):
+     if not (1.00 <= price <= 45.00):
+         raise ValueError("Price must be between 1.00 and 45.00")
+     return price
+        
 
     def __repr__(self):
         return f"<Cheese {self.id}>"
